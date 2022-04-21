c0cbb3d3593f1549fff6f26bd86c67b385e8fdca
dariomtz <gusdamtzr@gmail.com>
2022-04-20 20:11:02 -0500

    Built main screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blueGrey[900],
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Expanded(
+              child: Center(
+                  child: Text(
+            "Toque para escuchar",
+            style: TextStyle(color: Colors.white, fontSize: 17),
+          ))),
+          Expanded(
+            child: Ink(
+                decoration: const ShapeDecoration(
+                  color: Colors.white,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                    iconSize: 150,
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.music_note,
+                      color: Colors.purple,
+                    ))),
           ),
-        ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Ink(
+                    decoration: const ShapeDecoration(
+                        color: Colors.white, shape: CircleBorder()),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Ink(
+                    decoration: const ShapeDecoration(
+                        color: Colors.white, shape: CircleBorder()),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.power_settings_new_sharp,
+                          color: Colors.black,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
